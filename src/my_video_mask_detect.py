2dab49f9622bb5a1b6a78f8dde230f55418cfd00
ldaniel <contato@leandrodaniel.com>
2020-07-14 14:18:46 -0300

    Update mask detection script

--- src/my_video_mask_detect.py
+++ src/my_video_mask_detect.py
@@ -1,26 +1,25 @@
 import cv2
 import numpy as np
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import load_model
 
-with open('../models/model_CNN.json', 'r') as f:
-    loaded_model_json = f.read()
-model = model_from_json(loaded_model_json)
-model.load_weights("../models/model_CNN.h5")
+model = load_model("../models/model_NN.h5")
 print("Loaded model from disk")
 
 resMap = {
     0: 'Mask detected',
-    1: 'Mask not detected'
+    1: 'Mask not detected',
+    2: 'Unknown'
 }
 
 colorMap = {
     0: (0, 255, 0),
-    1: (0, 0, 255)
+    1: (0, 0, 255),
+    2: (255, 0, 0),
 }
 
 
 def prepare_image(pth):
-    return cv2.resize(pth, (224, 224)).reshape(1, 224, 224, 3) / 255.0
+    return cv2.resize(pth, (256, 256)).reshape(-1, 256, 256, 1) / 255.0
 
 
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -34,6 +33,8 @@ while True:
         slicedImg = img[face[1]:face[1] + face[3], face[0]:face[0] + face[2]]
         prediction = model.predict(prepare_image(img))
         prediction = np.argmax(prediction)
+
+        print(prediction)
 
         cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), colorMap[prediction], 2)
         cv2.putText(img, resMap[prediction], (face[0], face[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
