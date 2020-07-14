0b779d2cbf142d035216a8492c8e3b76b19df346
ldaniel <contato@leandrodaniel.com>
2020-07-14 14:22:02 -0300

    Update mask detection script

--- src/my_video_mask_detect.py
+++ src/my_video_mask_detect.py
@@ -2,16 +2,17 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model("../models/model_NN.h5")
+#model = load_model("../models/model_NN.h5")
+model = load_model("../models/model_CNN.h5")
 print("Loaded model from disk")
 
-resMap = {
+pred_label = {
     0: 'Mask detected',
     1: 'Mask not detected',
     2: 'Unknown'
 }
 
-colorMap = {
+pred_color = {
     0: (0, 255, 0),
     1: (0, 0, 255),
     2: (255, 0, 0),
@@ -36,8 +37,8 @@ while True:
 
         print(prediction)
 
-        cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), colorMap[prediction], 2)
-        cv2.putText(img, resMap[prediction], (face[0], face[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+        cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), pred_color[prediction], 2)
+        cv2.putText(img, pred_label[prediction], (face[0], face[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
 
     cv2.imshow('FaceMask Detection', img)
     if cv2.waitKey(1) & 0xff == ord('q'):
